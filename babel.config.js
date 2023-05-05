8ba6695369ccb07ff264cec1b30285db71e5cc70
AlanChou1995 <99056343+AlanChou1995@users.noreply.github.com>
2023-05-05 20:34:29 +0800

    first commit hahaha

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,30 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ["@babel/plugin-transform-flow-strip-types"],
+      ["@babel/plugin-proposal-decorators", { "legacy": true }],
+      ["@babel/plugin-proposal-class-properties", { "loose": true }],
+      // ["module:react-native-dotenv", {
+      //   "envName": "APP_ENV",
+      //   "moduleName": "@env",
+      //   "path": ".env",
+      // }],
+      ["module-resolver", {
+        "alias": {
+          "@Components": "./app/components",
+          "@Styles": "./app/styles",
+          "@Hooks": "./app/hooks",
+          "@Types": ".app/types"
+        },
+        "extensions": [
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx",
+        ]
+      }],
+    ]
   };
 };
